688086c839b15b17b447b313bea1290c8eaf880e
Marcos de Sá <sa.marcos@yahoo.com.br>
2016-06-04 17:48:12 -0300

    Getting started - Adding tasks

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ Todos = new Mongo.Collection("todos");
 
 if(Meteor.isClient){
     // client code goes here
+    Template.todos.helpers({
+    	todo(){
+    		return Todos.find({}, { sort: { createdAt: -1}});
+    	}
+    });
+    Template.addTodo.events({
+    	"submit form": function(event){
+    		event.preventDefault();
+    		let todoName = $("[name='todoName']").val(); // jQuery
+    		//let todoName = event.target.todoName.value; // non jQuery
+    		Todos.insert({
+    			name: todoName,
+    			completed: false,
+    			createdAt: new Date()
+    		});
+    		$("[name='todoName']").val("");
+    	}
+    });
 }
 
 if(Meteor.isServer){
