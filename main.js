715f3b3fdd0ae3ea149fd93ea7b26e6b57631173
Marcos de Sá <sa.marcos@yahoo.com.br>
2016-06-08 11:44:05 -0300

    User Accounts - Error Handling, Part 2

--- main.js
+++ main.js
@@ -130,6 +130,11 @@ if(Meteor.isClient){
             Accounts.createUser({
                 email,
                 password
+            }, function(error){
+                if(error)
+                    console.log(error.reason); // Output error if registration fails
+                else
+                    Router.go('home'); // Redirect user if registration succeeds
             });
             Router.go('home');
         }
