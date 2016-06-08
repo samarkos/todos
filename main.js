17d8a349ea1db731c96010c7787c3411465d65f9
Marcos de Sá <sa.marcos@yahoo.com.br>
2016-06-08 11:38:47 -0300

    User Accounts - Error Handling, Part 1

--- main.js
+++ main.js
@@ -139,7 +139,12 @@ if(Meteor.isClient){
             event.preventDefault();
             var email = $("[name=email]").val();
             var password = $("[name=password]").val();
-            Meteor.loginWithPassword(email, password);
+            Meteor.loginWithPassword(email, password, function(error){
+                if(error) 
+                    console.log(error.reason);
+                else
+                    Router.go('home');
+            });
         }
     });
 }
